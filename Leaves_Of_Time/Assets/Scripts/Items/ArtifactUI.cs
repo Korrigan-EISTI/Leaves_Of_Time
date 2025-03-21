fe68e2d26f35e41bb0c051d5f5ae6b33b530cc3a
An2sBRR <anissa.atc@gmail.com>
2025-03-21 11:07:23 +0100

    Canvas artefacts

--- Leaves_Of_Time/Assets/Scripts/Items/ArtifactUI.cs
+++ Leaves_Of_Time/Assets/Scripts/Items/ArtifactUI.cs
@@ -43,7 +43,7 @@ public class ArtifactUI : MonoBehaviour
         if (artifactText != null && player != null)
         {
             artifactText.text = $"{player.ArtifactsCollected}";
-           /* Debug.Log($"UI mise à jour : {player.ArtifactsCollected}/7");*/
+            Debug.Log($"UI mise à jour : {player.ArtifactsCollected}/7");
         }
         else
         {
